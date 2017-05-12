0d18e25c8ed624f49d4a1593ec4dc7388666905d
Dave Bechtum <dbechtum@gmail.com>
2017-05-12 16:11:56 +0200

    Added script and (temporary) scene for Trade Outpost

---
+++ Source Project/Assets/Scripts/Trade Outpost/TradeOutpost.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Completed;
+
+public class TradeOutpost : MonoBehaviour {
+	private GameObject player;
+	public void buyMedicine() 
+	{
+		//if (GetComponent<Player>().money >= 5){
+			
+			GetComponent<Player> ().infection--;
+		//	player.GetComponent<Player> ().money -= 5;
+		//}
+	}
+
+	public void buyFood()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		//if (GetComponent<Player>().money >= 1){
+
+		GameManager.instance.playerFoodPoints += 5;
+		//GameManager.instance.money--;
+		//}
+	}
+}

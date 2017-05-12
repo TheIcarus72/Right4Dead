8b2895ac92300bac3e32f0315cf3d6b53f4ef5d7
Dave Bechtum <dbechtum@gmail.com>
2017-05-12 11:23:01 +0200

    Added simple main menu, bug: pressing start brings u to day 2

---
+++ Source Project/Assets/Scripts/Main Menu/ButtonHover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonHover : MonoBehaviour {
+	public GameObject startButton;
+	public Sprite startIMG1, startIMG2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+		
+	void OnMouseEnter() {
+		startButton.GetComponent<SpriteRenderer> ().sprite = startIMG2;
+	}
+	void OnMouseExit() {
+		startButton.GetComponent<SpriteRenderer> ().sprite = startIMG1;
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("Hello World");
+		SceneManager.LoadScene(0);
+	}
+}
